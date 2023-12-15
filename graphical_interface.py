7e4f9a2a65492ad4b0e3ab878631170cc9b9f303
Paulius Padrostis <paulius.padrostis94@gmail.com>
2023-12-15 11:18:07 +0200

    Created a function to place 'x' or 'o' on buttons and check for current player and if square is empty.

--- graphical_interface.py
+++ graphical_interface.py
@@ -2,10 +2,28 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageFilter, ImageTk
 
+
+current_player = "X"
+
+
+# Create function to place 'x' or 'o' on buttons and check for current player and if square is empty.
+def update_board(button):
+    global current_player
+    if button["text"] == "":
+        if current_player == "X":
+            button.config(text="X")
+            current_player = "O"
+        else:
+            button.config(text="O")
+            current_player = "X"
+    else:
+        pass
+
+
 root = tk.Tk()  # Create main window
 root.geometry("1280x720")  # Set main window size to "1280x720"
-root.title("TIC-TAC-TOE") # Set the title of the main window
-root.resizable(False, False) # Lock the window size
+root.title("TIC-TAC-TOE")  # Set the title of the main window
+root.resizable(False, False)  # Lock the window size
 
 # Using PIL to blur out the background image
 image = Image.open("background.png")
@@ -23,31 +41,27 @@ board_frame = tk.Frame()
 board_frame.pack(pady=150)
 
 # First (bottom) row
-b1 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b1 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b1))
 b1.grid(row=2, column=0, padx=2, pady=2)
-b2 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b2 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b2))
 b2.grid(row=2, column=1, padx=2, pady=2)
-b3 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b3 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b3))
 b3.grid(row=2, column=2, padx=2, pady=2)
 
 # Second (middle) row
-b4 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b4 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b4))
 b4.grid(row=1, column=0, padx=2, pady=2)
-b5 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b5 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b5))
 b5.grid(row=1, column=1, padx=2, pady=2)
-b6 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b6 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b6))
 b6.grid(row=1, column=2, padx=2, pady=2)
 
 # Third (top) row
-b7 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b7 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b7))
 b7.grid(row=0, column=0, padx=2, pady=2)
-b8 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b8 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b8))
 b8.grid(row=0, column=1, padx=2, pady=2)
-b9 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge")
+b9 = tk.Button(master=board_frame, text="", width=16, height=8, relief="ridge", command=lambda: update_board(b9))
 b9.grid(row=0, column=2, padx=2, pady=2)
-
-
-
-
 
 root.mainloop()
